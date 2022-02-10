f0bde9570f2b73810224a1141c8e94ffdedf714f
xk08 <gronytzki@gmail.com>
2022-02-10 14:43:49 -0300

    Transcrevendo o exercicio para o Brasil

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+const registerBtn = document.querySelector(".register-btn");
+
+registerBtn.addEventListener("click", () => {
+  /// Escreva aqui o seu código, seguindo estas orientações passo a passo:
+
+  // 1. Obter o valor introduzido no input do e-mail.
+
+  // 2. Obter o valor introduzido no input de senha.
+
+  // 3. Obter o valor do input de rádio.
+
+  // 4. Obter o valor do input com o checkbox.
+
+  // 5. Validar se o utilizador for maior de idade. Caso contrário, mostramos
+  // uma mensagem de erro: "Tem de ser maior de idade para se registar no site".
+
+  // 6. Validamos se o utilizador aceitou os termos e condições. Caso contrário, exibimos
+  // uma mensagem de erro: "Deve aceitar os termos e condições para se registar no site".
+
+  // 7. Por fim, se tudo estiver correto, exibimos um objeto no console com as informações definidas pelo utilizador. 
+});
